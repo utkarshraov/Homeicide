e1f78decde28224bfdf1d0836a25123ae4e34b22
utkarshraov <hootkarsh@gmail.com>
2019-01-25 20:56:53 -0700

    Base level setup

---
+++ Homeicide/Assets/Scripts/cameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField]
+    private GameObject player;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position - Vector3.forward * 20.0f;
+    }
+}
