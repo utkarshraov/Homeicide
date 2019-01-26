059f869107d15c4556ae75b9be7ab1ccb02bf0c0
utkarshraov <hootkarsh@gmail.com>
2019-01-26 10:13:27 -0700

    added some camerashake and flamethrower

--- Homeicide/Assets/Scripts/cameraController.cs
+++ Homeicide/Assets/Scripts/cameraController.cs
@@ -8,14 +8,59 @@ public class cameraController : MonoBehaviour
 
     [SerializeField]
     private GameObject player;
+
+    [SerializeField]
+    private float trauma;
+
+    private float shake;
+
+    [SerializeField]
+    private float shakeDecay = 0.2f;
+
+    [SerializeField]
+    private float maxAngle;
+
+    [SerializeField]
+    private float maxOffset;
+
+    private Quaternion actualRotation;
+
     void Start()
     {
-        
+        actualRotation = transform.rotation;
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position = new Vector3(player.transform.position.x, transform.position.y, -40f);
+        Shake();
+    }
+
+    void Shake()
+    {
+        if (trauma < 0)
+            trauma = 0;
+        if (trauma > 1)
+            trauma = 1;
+        shake = trauma * trauma;
+        trauma -= shakeDecay * Time.deltaTime;
+
+        float angleX = maxAngle * shake * Random.Range(-1, 1);
+        float angleY = maxAngle * shake * Random.Range(-1, 1);
+        float angleZ = maxAngle * shake * Random.Range(-1, 1);
+        float angleW = maxAngle * shake * Random.Range(-1, 1);
+
+        //calculate translational shake
+        float offsetX = maxOffset * shake * Random.Range(-1, 1);
+        float offsetY = maxOffset * shake * Random.Range(-1, 1);
+
+        transform.position += new Vector3(offsetX, offsetY, 0);
+        transform.rotation = new Quaternion(actualRotation.x + angleX, actualRotation.y + angleY, actualRotation.z + angleZ, actualRotation.w + angleW);
+    }
+
+    public void addShake(float traumaValue)
+    {
+        trauma += traumaValue;
     }
 }
--- Homeicide/Assets/Scripts/playerController.cs
+++ Homeicide/Assets/Scripts/playerController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class playerController : MonoBehaviour
 {
     [SerializeField]
-    private int health;
+    private float health;
 
     [SerializeField]
     private int moveSpeed = 5;
@@ -19,6 +19,12 @@ public class playerController : MonoBehaviour
     [SerializeField]
     private Transform groundCheck;
 
+    [SerializeField]
+    private cameraController camera;
+
+    [SerializeField]
+    private GameObject flamethrowerPrefab;
+
     private bool isOnGround = true;
 
     void Start()
@@ -29,24 +35,43 @@ public class playerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        bool oldGround = isOnGround;
         isOnGround = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground")); // check for being on the ground
+
+        if (oldGround == false && isOnGround == true)
+            camera.addShake(0.2f); //shake the camera if we just hit the ground
+
+
         rb.velocity = new Vector2(Input.GetAxis("Horizontal") * moveSpeed, rb.velocity.y);
 
         if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
+            //jump
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             isOnGround = false;
         }
 
         if (rb.velocity.y < 0)
         {
+            //make the fall better
             rb.velocity += Vector2.up * Physics.gravity.y * 0.1f;
 
         }
     }
 
-    private void FixedUpdate()
+    public void TakeDamage(float damageValue)
     {
-       
+        health -= damageValue;
+    }
+
+
+    private IEnumerator Flamethrower()
+    {
+        yield return new WaitForSeconds(0.4f);
+
+
+
+
+        yield return null;
     }
 }
