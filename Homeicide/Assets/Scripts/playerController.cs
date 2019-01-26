5ea2a10ba14653d185ce348261d3594bca7f4081
utkarshraov <hootkarsh@gmail.com>
2019-01-26 11:39:15 -0700

    created flamethrower

--- Homeicide/Assets/Scripts/playerController.cs
+++ Homeicide/Assets/Scripts/playerController.cs
@@ -20,16 +20,31 @@ public class playerController : MonoBehaviour
     private Transform groundCheck;
 
     [SerializeField]
+    private Transform pointer;
+
+    [SerializeField]
     private cameraController camera;
+
+    [SerializeField]
+    private float flameOffset;
 
     [SerializeField]
     private GameObject flamethrowerPrefab;
 
+    [SerializeField]
+    private float flamethrowerDuration;
+
     private bool isOnGround = true;
+
+    private enum Direction { Left, Right }
+
+    Direction Facing = Direction.Right;
+
+    private bool lockDirection = false;
 
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -43,6 +58,7 @@ public class playerController : MonoBehaviour
 
 
         rb.velocity = new Vector2(Input.GetAxis("Horizontal") * moveSpeed, rb.velocity.y);
+        UpdateDirection();
 
         if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
@@ -55,7 +71,28 @@ public class playerController : MonoBehaviour
         {
             //make the fall better
             rb.velocity += Vector2.up * Physics.gravity.y * 0.1f;
+        }
 
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            StartCoroutine(Flamethrower());
+        }
+    }
+
+    void UpdateDirection()
+    {
+        if (!lockDirection)
+        {
+            if (rb.velocity.x > 0)
+            {
+                Facing = Direction.Right;
+                pointer.transform.position = new Vector3(0.5f * 8f, 0.3f * 8f) + transform.position;
+            }
+            else if (rb.velocity.x < 0)
+            {
+                Facing = Direction.Left;
+                pointer.transform.position = new Vector3(-0.5f * 8f, 0.3f * 8f) + transform.position;
+            }
         }
     }
 
@@ -67,10 +104,21 @@ public class playerController : MonoBehaviour
 
     private IEnumerator Flamethrower()
     {
-        yield return new WaitForSeconds(0.4f);
+        yield return new WaitForSeconds(0.4f); // animation time to setup chimney
 
+        GameObject flame = Instantiate(flamethrowerPrefab);
+        if (Facing == Direction.Left)
+            flame.transform.position = transform.position + new Vector3(-flameOffset, 0);
+        else if (Facing == Direction.Right)
+            flame.transform.position = transform.position + new Vector3(flameOffset, 0);
+        flame.transform.SetParent(transform);
+        lockDirection = true;
 
+        yield return new WaitForSeconds(flamethrowerDuration);
 
+        lockDirection = false;
+
+        Destroy(flame);
 
         yield return null;
     }
