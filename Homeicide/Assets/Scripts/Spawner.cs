46beba5a4d39482776497be95a86632ea3db3fad
josephDmoyle <u0929730@utah.edu>
2019-01-26 17:25:01 -0700

    Spawner

---
+++ Homeicide/Assets/Scripts/Spawner.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] GameObject enemy;
+
+    public void Spawn()
+    {
+        Instantiate(enemy, transform.position, Quaternion.identity);
+    }
+}
